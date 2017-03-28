a7e323efe19e179877afecbab6acaf228f4f8b2a
Mark Whalley <mark.whalley@hpe.com>
2017-03-28 14:47:19 +0100

    Removed components that have not been delivered in BDML

--- dump1090_sql/check_offsets.sh
+++
@@ -1,6 +0,0 @@
-echo "
-
-   select max(end_offset) as max_end_offset,  source_name from dump1090Scheduler.stream_microbatch_history group by 2 order by 2;
-
-   " | vsql
-   
--- dump1090_webdemo/dump1090_launch_visualiser.sh
+++
@@ -1,1 +0,0 @@
-./bin/dump1090_visualize.sh --action start --logfile /tmp/dump1090.log --schema dump1090_kafka --time_slice_number 60 --time_slice_units min --verbose Y --sleep 10
